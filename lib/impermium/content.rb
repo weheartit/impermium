b8c0de94d540aa3aa6b67b91480b6e5d0109dad3
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-08-28 18:05:13 +0200

    Implemented blog_entry API call

--- lib/impermium/content.rb
+++ lib/impermium/content.rb
@@ -1,5 +1,15 @@
 module Impermium
   module Content
+    def blog_entry(uid_ref, content, resource_url, enduser_ip, options={})
+      options.merge!(
+        :uid_ref => uid_ref,
+        :content => content,
+        :resource_url => resource_url,
+        :enduser_ip => enduser_ip
+        )
+      post("content/blog_entry", options)
+    end
+
     def comment(uid_ref, content, resource_url, enduser_ip, options={})
       options[:uid_ref] = uid_ref
       options[:content] = content
