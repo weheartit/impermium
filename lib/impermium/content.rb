d9ee58de5cef2a79733152c19921d04eff25237a
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-08-29 19:07:35 +0200

    Implemented rest of content API calls
    Similar API calls are dynamically defined.

--- lib/impermium/content.rb
+++ lib/impermium/content.rb
@@ -1,43 +1,62 @@
 module Impermium
   module Content
-    def blog_entry(uid_ref, content, resource_url, enduser_ip, options={})
+    [:blog_entry, :chatroom_message].each do |mname|
+      define_method(mname) do |uid_ref, content, resource_url, enduser_ip, options={}|
+        options.merge!(
+          :uid_ref => uid_ref,
+          :content => content,
+          :resource_url => resource_url,
+          :enduser_ip => enduser_ip
+          )
+        post("content/#{mname.to_s}", options)
+      end
+    end
+
+    [:chat_message, :message, :wall_message].each do |mname|
+      define_method(mname) do |uid_ref, uid_recv, content, enduser_ip, options={}|
+        options.merge!(
+          :uid_ref => uid_ref,
+          :uid_recv => uid_recv,
+          :content => content,
+          :enduser_ip => enduser_ip
+          )
+        post("content/#{mname.to_s}", options)
+      end
+    end
+    
+    [:comment, :forum_message].each do |mname|
+      define_method(mname) do |uid_ref, content, resource_url, enduser_ip, options = {}|
+        options.merge!(
+          :uid_ref => uid_ref,
+          :content => content,
+          :resource_url => resource_url,
+          :enduser_ip => enduser_ip
+          )
+        post("content/#{mname.to_s}", options)
+      end
+    end
+
+    def generic(uid_ref, ctype, content, resource_url, enduser_ip, options={})
       options.merge!(
         :uid_ref => uid_ref,
+        :ctype => ctype,
         :content => content,
         :resource_url => resource_url,
         :enduser_ip => enduser_ip
         )
-      post("content/blog_entry", options)
+      post("content/generic", options)
     end
-    
-    def chat_message(uid_ref, uid_recv, content, enduser_ip, options={})
+
+    def rating(uid_ref, rating, enduser_ip, options={})
+      raise ArgumentError, 'Rating is not in valid range' unless [1, 2, 3, 4, 5].include?(rating.to_i)
+      
       options.merge!(
         :uid_ref => uid_ref,
-        :uid_recv => uid_recv,
-        :content => content,
+        :rating => rating,
         :enduser_ip => enduser_ip
         )
-      post("content/chat_message", options)
+      post("content/rating", options)
     end
-    
-    def chatroom_message(uid_ref, content, resource_url, enduser_ip, options={})
-      options.merge!(
-        :uid_ref => uid_ref,
-        :content => content,
-        :resource_url => resource_url,
-        :enduser_ip => enduser_ip
-        )
-      post("content/chatroom_message", options)
-    end
-    
-    def comment(uid_ref, content, resource_url, enduser_ip, options={})
-      options.merge!(
-        :uid_ref => uid_ref,
-        :content => content,
-        :resource_url => resource_url,
-        :enduser_ip => enduser_ip
-        )
-      post("content/comment", options)
-    end
+
   end
 end
--- spec/impermium/content_spec.rb
+++ spec/impermium/content_spec.rb
@@ -145,4 +145,186 @@ describe "content comment events" do
     end
   end
 
+  describe "forum_message API call" do
+    describe "missing forum_message arguments" do
+      
+      it "should raise BadRequest error if 'enduser_ip' is missing" do
+        stub = stub_post("content/forum_message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.forum_message(@uid_ref, @content, @resource_url, "") }.should raise_error(Impermium::BadRequest)
+      end
+      
+      it "should raise BadRequest error if 'resource_url' is missing" do
+        stub = stub_post("content/forum_message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.forum_message(@uid_ref, @content, "", @enduser_ip) }.should raise_error(Impermium::BadRequest)
+      end
+      
+    end
+
+    describe "successful forum_message request" do
+      it "should flag 'just a comment' as not spam" do
+        stub = stub_post("content/forum_message").to_return(:status => 200, :body => fixture_content('forum_message'),
+          :headers => {:content_type => "application/json; charset=utf-8"})
+        res = Impermium.forum_message(@uid_ref, @content, @resource_url, @enduser_ip)
+        res.spam.confidence.should == "medium"
+        res.spam.label.should == "notspam"
+      end
+    end
+  end
+
+  describe "generic API call" do
+    describe "missing generic arguments" do
+      
+      it "should raise BadRequest error if 'enduser_ip' is missing" do
+        stub = stub_post("content/generic").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.generic(@uid_ref, "api testing request", @content, @resource_url, "") }.should raise_error(Impermium::BadRequest)
+      end
+      
+      it "should raise BadRequest error if 'resource_url' is missing" do
+        stub = stub_post("content/generic").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.generic(@uid_ref, "api testing request", @content, "", @enduser_ip) }.should raise_error(Impermium::BadRequest)
+      end
+      
+    end
+
+    describe "successful generic request" do
+      it "should flag 'just a comment' as not spam" do
+        stub = stub_post("content/generic").to_return(:status => 200, :body => fixture_content('generic'),
+          :headers => {:content_type => "application/json; charset=utf-8"})
+        res = Impermium.generic(@uid_ref, "api testing request", @content, @resource_url, @enduser_ip)
+        res.spam.confidence.should == "medium"
+        res.spam.label.should == "notspam"
+      end
+    end
+  end
+
+  describe "message API call" do
+    describe "missing message arguments" do
+      
+      it "should raise BadRequest error if 'enduser_ip' is missing" do
+        stub = stub_post("content/message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.message(@uid_ref, @uid_recv, @content, "") }.should raise_error(Impermium::BadRequest)
+      end
+      
+      it "should raise BadRequest error if 'uid_recv' is missing" do
+        stub = stub_post("content/message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.message(@uid_ref, "", @content, @enduser_ip) }.should raise_error(Impermium::BadRequest)
+      end
+      
+    end
+
+    describe "successful message request" do
+      it "should flag 'just a comment' as not spam" do
+        stub = stub_post("content/message").to_return(:status => 200, :body => fixture_content('message'),
+          :headers => {:content_type => "application/json; charset=utf-8"})
+        res = Impermium.message(@uid_ref, @uid_recv, @content, @enduser_ip)
+        res.spam.confidence.should == "medium"
+        res.spam.label.should == "notspam"
+      end
+    end
+  end
+
+  describe "rating API call" do
+    describe "missing rating arguments" do
+      
+      it "should raise BadRequest error if 'enduser_ip' is missing" do
+        stub = stub_post("content/rating").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.rating(@uid_ref, 1, "") }.should raise_error(Impermium::BadRequest)
+      end
+      
+      it "should raise BadRequest error if 'uid_ref' is missing" do
+        stub = stub_post("content/rating").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'uid_ref')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.rating("", 1, @enduser_ip) }.should raise_error(Impermium::BadRequest)
+      end
+      
+    end
+
+    describe "bad rating value" do
+      it "should raise ArgumentError for invalid rating value" do
+        stub = stub_post("content/rating").to_return(
+          :status => 400,
+          :body => "400",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.rating(@uid_ref, 0, @enduser_ip) }.should raise_error(ArgumentError)
+      end
+    end
+        
+    describe "successful rating request" do
+      it "should return simple response object" do
+        stub = stub_post("content/rating").to_return(:status => 200, :body => fixture_content('rating'),
+          :headers => {:content_type => "application/json; charset=utf-8"})
+        res = Impermium.rating(@uid_ref, 5, @enduser_ip)
+        res.should respond_to(:ts)
+        res.should respond_to(:etype)
+        res.should_not respond_to(:spam)
+      end
+    end
+  end
+
+  describe "wall_message API call" do
+    describe "missing wall_message arguments" do
+      
+      it "should raise BadRequest error if 'enduser_ip' is missing" do
+        stub = stub_post("content/wall_message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.wall_message(@uid_ref, @uid_recv, @content, "") }.should raise_error(Impermium::BadRequest)
+      end
+      
+      it "should raise BadRequest error if 'uid_recv' is missing" do
+        stub = stub_post("content/wall_message").to_return(
+          :status => 400,
+          :body => "400:Unable to verify the schema of the event: (Value '' is not of type 'string' for element 'enduser_ip')",
+          :headers => { :content_type => "application/json; charset=utf-u" }
+          )
+        lambda { Impermium.wall_message(@uid_ref, "", @content, @enduser_ip) }.should raise_error(Impermium::BadRequest)
+      end
+      
+    end
+
+    describe "successful wall_message request" do
+      it "should flag 'just a comment' as not spam" do
+        stub = stub_post("content/wall_message").to_return(:status => 200, :body => fixture_content('wall_message'),
+          :headers => {:content_type => "application/json; charset=utf-8"})
+        res = Impermium.wall_message(@uid_ref, @uid_recv, @content, @enduser_ip)
+        res.spam.confidence.should == "medium"
+        res.spam.label.should == "notspam"
+      end
+    end
+  end
+
 end
