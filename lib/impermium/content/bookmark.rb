c3593bd6e76a3ec3156b0e19c27edcd138c32594
Juanjo Bazán <jjbazan@gmail.com>
2012-03-05 12:27:02 +0100

    [Content] bookmark_like method

--- lib/impermium/content/bookmark.rb
+++ lib/impermium/content/bookmark.rb
@@ -11,5 +11,16 @@ module Impermium
       post("bookmark", options, &block)
     end
     
+    def bookmark_like(user_id, bookmark_id, bookmark_url, like_value, enduser_ip, options={}, &block)
+      options.merge!(
+        :user_id => user_id,
+        :bookmark_id => bookmark_id,
+        :bookmark_url => bookmark_url,
+        :like_value => like_value,
+        :enduser_ip => enduser_ip
+        )
+      post("bookmark/like", options, &block)
+    end
+    
   end
 end
--- spec/impermium/content/bookmark_spec.rb
+++ spec/impermium/content/bookmark_spec.rb
@@ -8,6 +8,7 @@ describe "content API section" do
       @ip = "2.2.2.2"
       @bookmark_id = "b00km4rk_ID"
       @bookmark_url = "http://example.com/category/book/marks/33?query=whi"
+      @like_value = "1"
     end
     
     describe "bookmark method" do
@@ -44,12 +45,61 @@ describe "content API section" do
 
       describe "successful request" do
         use_vcr_cassette
-        it "should mark bookmark post with 'notspam' label" do
+        it "should mark bookmark with 'notspam' label" do
           res = Impermium.bookmark(@user_id, @bookmark_id, @bookmark_url, @ip)
           res.spam_classifier.label.should == "notspam"
         end
       end
     end
+    
+    describe "bookmark_like method" do
+      describe "missing arguments" do
+        
+        describe "missing user_id" do
+          use_vcr_cassette
+          it "should raise BadRequest error" do
+            lambda { Impermium.bookmark_like(nil, @bookmark_id, @bookmark_url, @like_value, @ip) }.should raise_error(Impermium::BadRequest, /user_id/)
+          end
+        end
+      
+        describe "missing bookmark_id" do
+          use_vcr_cassette
+          it "should raise BadRequest error" do
+            lambda { Impermium.bookmark_like(@user_id, nil, @bookmark_url, @like_value, @ip) }.should raise_error(Impermium::BadRequest, /bookmark_id/)
+          end
+        end
+      
+        describe "missing bookmark_url" do
+          use_vcr_cassette
+          it "should raise BadRequest error" do
+            lambda { Impermium.bookmark_like(@user_id, @bookmark_id, nil, @like_value, @ip) }.should raise_error(Impermium::BadRequest, /bookmark_url/)
+          end
+        end
+        
+        describe "missing like_value" do
+          use_vcr_cassette
+          it "should raise BadRequest error" do
+            lambda { Impermium.bookmark_like(@user_id, @bookmark_id, @bookmark_url, nil, @ip) }.should raise_error(Impermium::BadRequest, /like_value/)
+          end
+        end
+        
+        describe "missing enduser_ip" do
+          use_vcr_cassette
+          it "should raise BadRequest error" do
+            lambda { Impermium.bookmark_like(@user_id, @bookmark_id, @bookmark_url, @like_value, nil) }.should raise_error(Impermium::BadRequest, /enduser_ip/)
+          end
+        end
+      end
+
+      describe "successful request" do
+        use_vcr_cassette
+        it "should mark like with 'notspam' label" do
+          res = Impermium.bookmark_like(@user_id, @bookmark_id, @bookmark_url, @like_value, @ip)
+          res.spam_classifier.label.should == "notspam"
+        end
+      end
+    end
+    
     
   end
 end
