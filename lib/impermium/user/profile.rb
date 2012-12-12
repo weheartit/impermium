4df71ab3017f1b2430eabebac33b7c8a6a3371ad
Antono Vasiljev <self@antono.info>
2012-12-13 02:25:22 +0300

    Impermium.profile overhaul according to API 4.0 docs
      - removed profile_user_feedback endpoint, dropping our methods and specs
      - updated method signatures, only mandatory attributes used, other
        stuff should go to options
      - updated VCR cassetes

--- lib/impermium/user/profile.rb
+++ lib/impermium/user/profile.rb
@@ -1,35 +1,18 @@
 module Impermium
   module Profile
-    REPORTER_USER_TYPE_VALUES = ["ENDUSER", "MODERATOR"] # => default first
 
-    def profile(user_id, profile_id, enduser_ip, options={}, &block)
-      options.merge!(
-        :user_id => user_id,
-        :profile_id => profile_id,
-        :enduser_ip => enduser_ip
-        )
+    def profile(user_id, profile_id, options={}, &block)
+      options.merge!(:user_id => user_id, :profile_id => profile_id)
       api_post("profile", options, &block)
     end
     
-    def profile_analyst_feedback(profile_id, analyst_id, desired_result, options={}, &block)
+    def profile_analyst_feedback(user_id, profile_id, desired_result, options={}, &block)
       options.merge!(
         :profile_id => profile_id,
-        :analyst_id => analyst_id,
-        :desired_result => desired_result
-        )
+        :user_id => user_id,
+        :desired_result => { Configuration::DEFAULT_API_VERSION => desired_result }
+      )
       api_post("profile/analyst_feedback", options, &block)
     end
-    
-    def profile_user_feedback(profile_id, rep_usr_id, rep_usr_type, reporter_ip, desired_result, options={}, &block)
-      options.merge!(
-        :profile_id => profile_id,
-        :reporter_user_id => rep_usr_id,
-        :reporter_user_type => REPORTER_USER_TYPE_VALUES.include?(rep_usr_type) ? rep_usr_type : REPORTER_USER_TYPE_VALUES.first,
-        :reporter_ip => reporter_ip,
-        :desired_result => desired_result
-        )
-      api_post("profile/user_feedback", options, &block)
-    end
-    
   end
 end
