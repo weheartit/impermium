307b93201cfe0b402c6651497a7949682a4a8b7a
Antono Vasiljev <self@antono.info>
2012-12-13 19:21:36 +0300

    Added Impermium.message_analyst_feedback, Impermium.message_user_feedback

--- lib/impermium/messaging/message.rb
+++ lib/impermium/messaging/message.rb
@@ -8,5 +8,23 @@ module Impermium
       )
       api_post('message', options, &block)
     end
+
+    def message_analyst_feedback(user_id, message_id, desired_result, options = {}, &block)
+      options.merge!(
+        :user_id => user_id,
+        :message_id => message_id,
+        :desired_result => { Configuration::DEFAULT_API_VERSION => desired_result }
+      )
+      api_post('message/analyst_feedback', options, &block)
+    end
+
+    def message_user_feedback(user_id, message_id, desired_result, options = {}, &block)
+      options.merge!(
+        :user_id => user_id,
+        :message_id => message_id,
+        :desired_result => { Configuration::DEFAULT_API_VERSION => desired_result }
+      )
+      api_post('message/user_feedback', options, &block)
+    end
   end
 end
--- spec/impermium/messaging/message_spec.rb
+++ spec/impermium/messaging/message_spec.rb
@@ -6,26 +6,33 @@ describe Impermium do
   let(:message_id)       { "123456" }
   let(:message_content)  { "fuck the police" }
   let(:analyst_id)       { "4n4l1s7" }
-  let(:reporter_user_id) { "r3p0rt3r" }
-  let(:ip)               { "1.1.1.1" }
   let(:content)          { "just a simple post" }
-  let(:post_id)          { "just-a-post#984721" }
-  let(:permalink)        { "http://example.com/2012/03/05/just_a_post" }
-  let(:url)              { "http://example.com" }
-  let(:desired_result)   { {:spam_classifier => { :label => "notspam" }} }
+  let(:desired_result)   { {:tags => [ "spam" ]} }
 
   describe ".message" do
-    describe "mandatory arguments" do
-      context "when user_id missing" do
-        use_vcr_cassette
-        it "should raise BadRequest error" do
-          pending "Not really mandatory"
-          -> { subject.message(nil, message_id, message_content) }.should raise_error(Impermium::BadRequest, /user_id/)
-        end
-      end
+    use_vcr_cassette
+
+    it "returns an instance of Impermium::Response" do
+      Impermium.message(@user_id, @message_id, @message_content)
+        .should be_a_kind_of(Impermium::Response)
     end
   end
 
-  describe ".message_analyste_feedback" do
+  describe ".message_analyst_feedback" do
+    use_vcr_cassette
+
+    it "returns an instance of Impermium::Response" do
+      Impermium.message_analyst_feedback(@user_id, @message_id, @desired_result)
+        .should be_a_kind_of(Impermium::Response)
+    end
+  end
+
+  describe ".message_user_feedback" do
+    use_vcr_cassette
+
+    it "returns an instance of Impermium::Response" do
+      Impermium.message_user_feedback(@user_id, @message_id, @desired_result)
+        .should be_a_kind_of(Impermium::Response)
+    end
   end
 end
